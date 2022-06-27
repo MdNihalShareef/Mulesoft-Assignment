997c375b332be68f7c141c309dcacfb6903c7db8
Md. Nihal Shareef <75412386+MdNihalShareef@users.noreply.github.com>
2022-06-27 17:38:13 +0530

    Update DmlOperations.java

--- src/sqlite_operations/DmlOperations.java
+++ src/sqlite_operations/DmlOperations.java
@@ -9,11 +9,19 @@ public class DmlOperations {
 			Class.forName("org.sqlite.JDBC");
 			Connection conn = DriverManager.getConnection("jdbc:sqlite:E:/Program Files/sqlite/mydb.db");
 			Statement stmt= conn.createStatement();
-			stmt.executeUpdate("create table movies(mv_name varhar(10), actor varhar(10), actress varhar(10), release_year int, dir_name varhar(10))");
+			stmt.executeUpdate("create table movies(mv_name varhar(20), actor varhar(20), actress varhar(20), release_year int, dir_name varhar(20))");
 			stmt.executeUpdate("insert into movies values('kgf','yash','srinidhi',2019,'prashant')");
-			stmt.executeUpdate("insert into movies values('rrr','ram charan','aliya',2022,'rajamouli')");
+			stmt.executeUpdate("insert into movies values('rrr','ram charan','aliya bhatt',2022,'rajamouli')");
+			stmt.executeUpdate("insert into movies values('john wick','keanu reaves','Bridget',2014,'Chad')");
+			stmt.executeUpdate("insert into movies values('premam','nivin pauly','sai pallavi',2015,'Alphonse')");
+			stmt.executeUpdate("insert into movies values('vikram','kamal hasan','Swathishta',2022,'lokesh')");
 			
 			ResultSet rs=stmt.executeQuery("select * from movies");
+			while (rs.next()) {
+				System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getInt(4)+" "+rs.getString(5));
+			}
+			System.out.println("Yash Movies");
+			rs=stmt.executeQuery("select * from movies where actor='yash'");
 			while (rs.next()) {
 				System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getInt(4)+" "+rs.getString(5));
 			}
